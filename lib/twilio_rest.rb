dfa49e37b4cf34064639088ec427971f49e1c4cc
etika ahuja <etika@9piecesof8.com>
2019-04-19 22:03:41 +0530

    changes

--- lib/twilio_rest.rb
+++ lib/twilio_rest.rb
@@ -31,7 +31,6 @@ end
 end
 
 def set_category(message)
-  binding.remote_pry
   event =Event.create(message: message.body,start_date: message.date_sent)
   Keyword.all.each do|keyword|
     if event.message.include?(keyword.name)
