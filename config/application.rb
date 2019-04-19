ee7440b28b1cbf1b36033475789ca2147f85f036
etika ahuja <etika@9piecesof8.com>
2019-04-19 09:16:28 +0530

    Changes in email settings

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,15 @@ module Khushibaby
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     APP_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/app_config.yml")).result)[Rails.env]
-
+    config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      :user_name => APP_CONFIG['email'],
+      :password => APP_CONFIG['pass'],
+      authentication: 'plain',
+      :enable_starttls_auto => true
+     }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
