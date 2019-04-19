b33ca65f388af5b90743e1ddb8f85a49d909c40f
etika ahuja <etika@9piecesof8.com>
2019-04-19 15:31:43 +0530

    changes

--- lib/twilio_rest.rb
+++ lib/twilio_rest.rb
@@ -18,13 +18,13 @@ def listing_whatsapp_message
 
    @messages = @twilio_client.messages.list(
                               to:'whatsapp:+14155238886',
-                              date_sent_after:  Event.last.present? ? Event.last.sent_date : Date.today
+                              date_sent_after:  Event.last.present? ? Event.last.start_date : Date.today
                            )
 
  @messages.each do |m|
   phone_number =m.from.split("+")[1]
   user = User.find_by_phone_number(phone_number)
-  if m.media_url.nil? && u.account_activated?
+  if  user.account_activated?
     set_category(m)
   end
 end
